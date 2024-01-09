f628c783ff5c683bc7d1ceced7e0ac1f633fcf7b
a-jmal <ajmalka1983@gmail.com>
2024-01-09 18:01:02 +0530

    mainj

---
+++ AEYE/app/src/main/java/com/example/aeye/userselection.java
@@ -0,0 +1,14 @@
+package com.example.aeye;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class userselection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_userselection);
+    }
+}
