77a1cf07268bb2f01b32cfd9be1c04e28cac4b38
a-jmal <ajmalka1983@gmail.com>
2024-01-11 18:27:05 +0530

    lib

---
+++ A_EYE/A_EYE/app/src/main/java/com/example/a_eye/userselection.java
@@ -0,0 +1,14 @@
+package com.example.a_eye;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class userselection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_userselection);
+    }
+}
